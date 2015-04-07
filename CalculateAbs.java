c04ba5611ea911eb9589bc34ae7bdd9e4de9fd98
MaxwellSt <st_maxwell_@mail.ru>
2015-04-07 11:24:37 +0300

    Create CalculateAbs.java

---
+++ CalculateAbs.java
@@ -0,0 +1,27 @@
+package package_01;
+
+public abstract class CalculateAbs {
+
+	public abstract void sum(int a, int b);
+
+	public abstract void sum(float a, float b);
+
+	public abstract void minus(int a, int b);
+
+	public abstract void minus(float a, float b);
+
+	public abstract void multiply(int a, int b);
+
+	public abstract void multiply(float a, float b);
+
+	public abstract void division(int a, int b);
+
+	public abstract void division(float a, float b);
+
+	public abstract void print(float a, float b);
+
+	public abstract void print(int a, int b);
+
+	public abstract int min(int... param);
+
+}
